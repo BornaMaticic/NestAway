11bf799fab0f04f582cae3f3f5858e825aee1986
Borna Maticic <bornamaticic@yahoo.com>
2019-02-01 16:53:04 +0000

    back end initial setup.

---
+++ backend/src/main/java/com/codeclan/NestAway/NestAway/NestAwayApplication.java
@@ -0,0 +1,14 @@
+package com.codeclan.NestAway.NestAway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestAwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NestAwayApplication.class, args);
+	}
+
+}
+
