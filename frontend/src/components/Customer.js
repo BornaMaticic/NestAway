bb4037d9b78240a5f79d9ddd26877e8b1fb06b5c
Christina Davies <christinahdavies@gmail.com>
2019-02-06 20:26:22 +0000

    added cards for Customer list

--- frontend/src/components/Customer.js
+++ frontend/src/components/Customer.js
@@ -1,13 +1,30 @@
 import React from 'react';
-
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
 const Customer = (props) => {
-  console.log(props);
-  const { firstName, lastName } = props.customer
 
   return (
     <div className="customer">
-      <p>Customer: {firstName} {lastName}</p>
+      <Card>
+        <CardContent>
+          <Typography>
+            Name: {props.customer.firstName} {props.customer.lastName}
+          </Typography>
+        </CardContent>
+
+        <CardActions>
+          <Button size="small"
+          href={`http://localhost:8080/api/customer/${props.customer.id}`}
+          target="_blank">
+          View details
+          </Button>
+        </CardActions>
+      </Card>
     </div>
   )
 }
